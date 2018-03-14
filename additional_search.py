c8b29e476288d4f54e85a378826318a2f35c6f07
vasilmao <vasezs@inbox.ru>
2018-03-14 20:17:57 +0400

    updated

--- additional_search.py
+++ additional_search.py
@@ -8,5 +8,5 @@ def main():
     toponym_to_find = "Саратов, Большая Горная, 1"
     # Показываем карту с фиксированным масштабом.
     lat, lon = get_coordinates(toponym_to_find)
-    ll_spn = "ll={0},{1}&spn=0.005,0.005".format(lat, lon)
-    show_map(ll_spn, "map")
+    ll = "{0},{1}".format(lat, lon)
+    show_map(ll=ll, map_type="map")
--- mapapi.py
+++ mapapi.py
@@ -26,29 +26,23 @@ def form_request(**kwargs):
             x += i + "=" + str(kwargs[i])
             x += '&'
         x = x[:-1:]
+    print(x)
     return x
 
 
-def map_bigger(map_request, delta, spnx, spny, ll):
-    if spnx + delta > 0.005 and spny + delta > 0.005:
-        #print(map_request)
-        newx = round(spnx+delta, 5)
-        newy = round(spny+delta, 5)
-        spnx = newx
-        spny = newy
-        spn=str(newx) + ',' + str(newy)
-        #print(map_request)
-    return form_request(ll=ll, spn=spn), spnx, spny
+def map_bigger(map_request, delta, sizex, sizey, ll, type, z):
+    if 0 <= z + delta <= 17:
+        z += delta
+    return form_request(ll=ll, l=type, size=str(sizex)+','+str(sizey), z=z), z
 
 
-def show_map(ll_spn=None, map_type="map", add_params=None):
-    if ll_spn:
-        map_request = "http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}".format(**locals())
-        spn = ll_spn.split('&')[1][4:]
-        spnx, spny = map(float, spn.split(','))
+def show_map(ll=None, z=17, sizex=600, sizey=450, map_type='map', add_params=None):
+    z = 1
+    if ll:
+        map_request = "http://static-maps.yandex.ru/1.x/?ll={0}&z={1}&size={2},{3}&l={4}".format(ll, z, sizex, sizey, map_type)
+        print(map_request)
     else:
         map_request = "http://static-maps.yandex.ru/1.x/?l={map_type}".format(**locals())
-        spnx, spny = 0.005, 0.005
 
     if add_params:
         map_request += "&" + add_params
@@ -61,9 +55,8 @@ def show_map(ll_spn=None, map_type="map", add_params=None):
         sys.exit(1)
 
     # Инициализируем pygame
-    ll = ll_spn.split('&')[0][3::]
     pygame.init()
-    screen = pygame.display.set_mode((600,450))
+    screen = pygame.display.set_mode((600, 450))
     map_file = form_map(response)
     screen.blit(pygame.image.load(map_file), (0, 0))
     # Рисуем картинку, загружаемую из только что созданного файла.
@@ -74,13 +67,12 @@ def show_map(ll_spn=None, map_type="map", add_params=None):
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEUP:
-                new_req, spnx, spny = map_bigger(map_request, 0.01, spnx, spny, ll)
-                print(new_req, spnx, spny)
+                new_req, z = map_bigger(map_request, 1, sizex, sizey, ll, map_type, z)
                 response = requests.get(new_req)
                 map_file = form_map(response)
                 screen.blit(pygame.image.load(map_file), (0, 0))
             if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEDOWN:
-                new_req, spnx, spny = map_bigger(map_request, -0.01, spnx, spny)
+                new_req, z = map_bigger(map_request, -1, sizex, sizey, ll, map_type, z)
                 response = requests.get(new_req)
                 map_file = form_map(response)
                 screen.blit(pygame.image.load(map_file), (0, 0))
