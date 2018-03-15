c44e912be397c399724793920f6a1aa8905e613b
vasilmao <vasezs@inbox.ru>
2018-03-15 19:27:47 +0400

    added classes

--- mapapi.py
+++ mapapi.py
@@ -5,9 +5,14 @@ import sys
 import os
 
 class Map:
-    def __init__(self, map_file, x, y, sizex, sizey, map_type, z, add_params):
-        self.map_file, self.x, self.y, self.sizex, self.sizey, self.l, self.z, self.params = map_file, x, y, sizex, sizey, map_type, z, add_params
-        self.request = self.form_request(self.params, ll=str(x)+','+str(y), size=str(sizex) + ',' + str(sizey), l=map_type, z=z)
+    def __init__(self, map_file, ll, map_type, spnx, spny, add_params):
+        self.map_file = map_file
+        self.ll = ll
+        self.spnx = spnx
+        self.spny = spny
+        self.l = map_type
+        self.params = add_params
+        self.request = self.form_request(self.params, ll=ll, spn=str(spnx) + ',' + str(spny), l=map_type)
         self.map_file = self.form_map(requests.get(self.request))
 
     def form_request(self, args, **kwargs):
@@ -29,6 +34,15 @@ class Map:
         print(x)
         return x
 
+    def request_map(self, request):
+        response = requests.get(request)
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(self.request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+        return response
+
     def form_map(self, response):
         # Запишем полученное изображение в файл.
         try:
@@ -40,20 +54,30 @@ class Map:
 
         return self.map_file
 
-    def change_size(self, d=0):
-        if 0<= self.z + d <= 17:
-            self.z += d
-        self.request = self.form_request(self.params, ll=str(self.x)+','+str(self.y), size=str(self.sizex) + ',' + str(self.sizey), l=self.l, z=self.z)
-        self.map_file = self.form_map(requests.get(self.request))
+    def map_change_size(self, delta):
+        # Изменяем параметр spn.
+        self.spnx = round(self.spnx * delta, 3)
+        self.spnx = min(2, max(0.002, self.spnx))
+        self.spny = round(self.spny * delta, 3)
+        self.spny = min(2, max(0.002, self.spny))
+        self.request = self.form_request(self.params, ll=self.ll, l=self.l, spn=str(self.spnx) + ',' + str(self.spny))
+        response = self.request_map(self.request)
+        self.map_file = self.form_map(response)
 
-def show_map(ll=None, z=17, sizex=600, sizey=450, map_type='map', add_params=None):
-    ll_h = list(map(float, ll.split(',')))
-    x = ll_h[0]
-    y = ll_h[1]
-    cur_map = Map('map.png', x, y, sizex, sizey, map_type, 17, [])
+    def move_map(self, move_x, move_y):
+        # Изменяем параметр ll.
+        new_ll = '{},{}'.format(float(self.ll.split(',')[0]) + move_x, float(self.ll.split(',')[1]) + move_y)
+        self.ll = new_ll
+        self.request = self.form_request(self.params, ll=self.ll, l=self.l, spn=str(self.spnx) + ',' + str(self.spny))
+        response = self.request_map(self.request)
+        self.map_file = self.form_map(response)
+        # Возвращаем запрос карты с измененным ll.
+
+def show_map(ll=None, z=17, spnx=0.02, spny=0.02, map_type='map', add_params=[]):
+    cur_map = Map(map_file='map.png', ll=ll, map_type=map_type, spnx=spnx, spny=spny, add_params=add_params)
     # Инициализируем pygame
     pygame.init()
-    screen = pygame.display.set_mode((sizex, sizey))
+    screen = pygame.display.set_mode((600, 450))
     screen.blit(pygame.image.load(cur_map.map_file), (0, 0))
     # Рисуем картинку, загружаемую из только что созданного файла.
     # Переключаем экран и ждем закрытия окна.
@@ -64,11 +88,18 @@ def show_map(ll=None, z=17, sizex=600, sizey=450, map_type='map', add_params=Non
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_PAGEUP:
-                    cur_map.change_size(1)
-                    screen.blit(pygame.image.load(cur_map.map_file), (0, 0))
+                    cur_map.map_change_size(2)
                 if event.key == pygame.K_PAGEDOWN:
-                    cur_map.change_size(-1)
-                    screen.blit(pygame.image.load(cur_map.map_file), (0, 0))
+                    cur_map.map_change_size(0.5)
+                if event.key == pygame.K_RIGHT:
+                    cur_map.move_map(cur_map.spnx * 4, 0)
+                if event.key == pygame.K_LEFT:
+                    cur_map.move_map(-cur_map.spnx * 4, 0)
+                if event.key == pygame.K_DOWN:
+                    cur_map.move_map(0, -cur_map.spny * 2)
+                if event.key == pygame.K_UP:
+                    cur_map.move_map(0, cur_map.spny * 2)
+                screen.blit(pygame.image.load(cur_map.map_file), (0, 0))
         pygame.display.flip()
 
     pygame.quit()
